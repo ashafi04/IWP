88c1d1104e0939cbd48de5aad9442bdc55837d0e
ashafi04 <ashafi04@gmail.com>
2023-10-30 15:38:16 +0800

    Added New C++ and BP BasicEnemy class
    + Enemy now moves with animation to target point inside player base

---
+++ Source/IWP/Public/BasicEnemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseUnit.h"
+#include "BasicEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IWP_API ABasicEnemy : public ABaseUnit
+{
+	GENERATED_BODY()
+	
+};
